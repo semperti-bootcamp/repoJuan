bfcb68e4f88de012126fa1dcb7f8c10d2c0bb70a
juantelez <elcorreodejuan@gmail.com>
2019-07-05 15:55:34 -0300

    Agregué el yaml para instalar el JDK 1.8 con Ansible

--- nuevaVM.tf
+++ nuevaVM.tf
@@ -3,6 +3,12 @@ provider "ovirt" {
   url = "http://rhev-m.semperti.local/ovirt-engine/api"
   password = "Semperti1!"
 }
+
+#resource "tls_self_signed_cert" "example" {
+#  key_algorithm   = "rsaEncryption"
+#  private_key_pem = "${file(\"100E.pem\")}"
+#}
+#
 resource "ovirt_vm" "my_vm" {
 name = "Terraform_fedora"
 
@@ -20,11 +26,11 @@ boot_proto = "dhcp"
 #
 template = "centos7-cloud"
 }
-
-resource "ovirt_disk" "my_disk" {
-name = "terraform_test"
-size = 4096
-format = "cow"
-storage_domain_id = "43d4405f-4760-471c-b8e2-54e187d762e1"
-sparse = true
-}
+#
+#resource "ovirt_disk" "my_disk" {
+#name = "terraform_test"
+#size = 4096
+#format = "cow"
+#storage_domain_id = "43d4405f-4760-471c-b8e2-54e187d762e1"
+#sparse = true
+#}
