db2c13045ba0b2edd31dfea679232aa948cb3880
juantelez <elcorreodejuan@gmail.com>
2019-08-12 22:38:10 -0300

    Arreglé las configuraciones para que clone mi repo y no el repo original

--- arreglarDirectorioUpload.sh
+++ arreglarDirectorioUpload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Scriptcito para poder tirar el maven sin tener que pasarle el directorio para los PDF
-export REPO=/semperti-bootcamp/week01
-sed -i s+/upload+$REPO/PDFs+ /root/semperti-bootcamp/week01/Code/src/main/java/com/semperti/trial/journals/Application.java
+export REPO=/repoJuan
+sed -i s+/upload+$REPO/PDFs+ /root/repoJuan/Code/src/main/java/com/semperti/trial/journals/Application.java
